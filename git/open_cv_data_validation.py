1149e9dc3f8d2b0d33d97dbeb5eeb6abfd0793d4
Derek Huang <huangd8@wit.edu>
2024-03-20 22:13:08 -0400

    Data Validation WIP

--- git/open_cv_data_validation.py
+++ git/open_cv_data_validation.py
@@ -1,6 +1,14 @@
 import cv2
 import numpy as np
 import time
+
+'''
+Will be utilizing a different color sticker for each point of interest
+Script will go through and create a mask for each point of interest
+It will measure the distance between the center of the stickers (green to orange) and (yellow to blue)
+Angle will be calculated based on the distance from the center of the stickers and displayed on the top left corner of the frame
+Law of Cosines will be used to calculate the angle
+'''
 # Define the minimum area for the objects to be detected
 min_area = 67^2
 
@@ -22,7 +30,9 @@ distance_num = 0
 centers = []
 prev_x = 0
 prev_y = 0
-
+def find_angles_and_display(frame, dist_yb, dist_go):
+    # Angle calculation and display onto frame
+    pass
 def find_object_centers(cx, cy, cw, ch):
     global centers
     global prev_x
@@ -68,8 +78,17 @@ def draw_centers_and_measure(frame, centers):
 
 # Define the lower and upper bounds of your object's color in HSV
 # These values should be adjusted based on the color of your objects
-color_lower = np.array([25, 100, 100])
-color_upper = np.array([35, 255, 255])
+color_lower_yellow = np.array([25, 100, 100])
+color_upper_yellow = np.array([35, 255, 255])
+
+color_lower_blue = np.array([110, 100, 100])
+color_upper_blue = np.array([130, 255, 255])
+
+color_lower_green = np.array([45, 100, 100])
+color_upper_green = np.array([75, 255, 255])
+
+color_lower_orange = np.array([10, 100, 100])
+color_upper_orange = np.array([25, 255, 255])
 
 # Start capturing video from the webcam
 cap = cv2.VideoCapture(0)
@@ -83,13 +102,41 @@ while True:
     # Convert the frame to the HSV color space
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-    # Create a mask for the specified color range
-    mask = cv2.inRange(hsv, color_lower, color_upper)
+    # Create a mask for the specified color range (yellow, blue, green, and orange)
+    yellow_mask = cv2.inRange(hsv, color_lower_yellow, color_upper_yellow)
+    blue_mask = cv2.inRange(hsv, color_lower_blue, color_upper_blue)
+    green_mask = cv2.inRange(hsv, color_lower_green, color_upper_green)
+    orange_mask = cv2.inRange(hsv, color_lower_orange, color_upper_orange)
+
+    # Combine the masks into a single mask
+    combined_mask1 = cv2.bitwise_or(yellow_mask, blue_mask)
+    compiled_mask2 = cv2.bitwise_or(green_mask, orange_mask)
+
 
     # Find contours in the image
-    contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    contours_yb, _ = cv2.findContours(combined_mask1, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    contours_go, _ = cv2.findContours(compiled_mask2, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-    for contour in contours:
+    # Go through and find the distances between the centers of the objects (yellow and blue), (green and orange)
+    for contour in contours_yb:
+        area = cv2.contourArea(contour)
+        if area > min_area:
+            # Object is larger than minimum area, so proceed with distance calculation
+            # Assuming you have the perceived width in pixels (e.g., from bounding box width)
+            x, y, w, h = cv2.boundingRect(contour)
+            perceived_width = w  # or use h for height, depending on known dimension
+            
+            # Draw the bounding box for visualization
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+            # Find the centers of objects
+            find_object_centers(x, y, w, h)
+            # Draw the centers and measure the distance
+            frame = draw_centers_and_measure(frame, centers)
+            print(centers)
+            print(prev_x)
+            print(prev_y)
+    for contour in contours_go:
         area = cv2.contourArea(contour)
         if area > min_area:
             # Object is larger than minimum area, so proceed with distance calculation
