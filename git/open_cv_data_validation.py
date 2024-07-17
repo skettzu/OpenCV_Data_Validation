ae5cdf5cac41b6b19b9af36b60175338be234f09
Derek Huang <huangd8@wit.edu>
2024-07-16 20:53:22 -0400

    All four points detected, manually measured bottom and top angles. Pending HSV values

--- git/open_cv_data_validation.py
+++ git/open_cv_data_validation.py
@@ -7,28 +7,18 @@ import time
 import threading
 
 '''
-WARNING: When using this test script, please block the webcam from any bg bright colors using your body or have a white/black piece of paper to 
-act as the background. Usage of a shadowbox/photobox is strongly encouraged for optimal performance.
-It is recommended to wear black medical gloves as well if manually driving finger. Not following this recommendation may result in
-inaccurate results.
-
 ** Closer the finger is to the camera, the more accurate the data
 '''
 
 '''
-Notes:
-Will be utilizing a different color sticker for each point of interest
-Script will go through and create a mask for each point of interest
-It will measure the distance between the center of the stickers (yellow to pink [top]) and (green to orange [bottom])
-Angle will be calculated based on the distance from the center of the stickers and displayed on the top left corner of the frame
-Law of Cosines will be used to calculate the angle
-
-
 To Do:
 - Use shadowbox and motorized test setup to validate the angle calculations [prio]
-- Validate the minimum/maximum area [wip]
-- Check if reducing the region of interest is necessary [wip]
+- Find a consistent HSV range for colors [prio]
 '''
+
+# Actual static angles:
+# YP: 112 degrees
+# GO: 104.5 degrees
 datetime_object = datetime.datetime.now()
 datetime_string = datetime_object.strftime("%Y_%m_%d_%H_%M_%S")
 print(datetime_string)
@@ -37,9 +27,9 @@ file_path = fr"C:\Users\huangd8\Desktop\OpenCV\Data_Trial_{datetime_string}.csv"
 # Angle data & timestamp to write to csv file
 data = [['Top angle:'], ['Bottom angle:'], [' '], ['Timestamp:']]
 
-# Define the minimum area for the objects to be detected
-min_area = 50
-max_area = 150
+# Define the minimum & maximum area for the objects to be detected
+min_area = 45
+max_area = 70
 
 # Your known width or height of the object
 KNOWN_WIDTH = 1.5875  # Width of sticker in millimeters (1/16 of inch)
@@ -48,55 +38,57 @@ KNOWN_WIDTH = 1.5875  # Width of sticker in millimeters (1/16 of inch)
 # KNOWN_DISTANCE = 230.0  # Distance from camera in millimeters
 
 # Your known static distance of top joint crevice side in mm (orange side from diagram) [yellow sticker]
-KNOWN_STATIC_TOP_DISTANCE1 = 7.0 # ~7 mm
+KNOWN_STATIC_TOP_DISTANCE1 = 6.7 # ~7 mm
 
 # Your known static distance of top joint crevice side in mm (blue side from diagram) [pink sticker]
-KNOWN_STATIC_TOP_DISTANCE2 = 9.5 # ~9.5 mm
+KNOWN_STATIC_TOP_DISTANCE2 = 9.2 # ~9.5 mm
 
 # Your known static distance of top joint crevice side in mm (orange side from diagram) [green sticker]
 KNOWN_STATIC_BOT_DISTANCE1 = 6.5 # ~6.5 mm
 # Your known static distance of top joint crevice side in mm (blue side from diagram) [orange sticker]
-KNOWN_STATIC_BOT_DISTANCE2 = 8.5 # ~8.5 mm
+KNOWN_STATIC_BOT_DISTANCE2 = 8.2 # ~8.5 mm
+
+# Initialize go and yp distances
+dist_go = 0
+dist_yp = 0
+# Initialize top and bottom angles
+top_angle = 0
+bot_angle = 0
 
 # The perceived width in pixels by OpenCV/Webcam
 perceived_width = 0
 
-# Threading flag
-flag = 0
 
 # Define the lower and upper bounds of your object's color in HSV
 # These values should be adjusted based on the color of your objects
-color_lower_yellow = np.array([28, 100, 90])
-color_upper_yellow = np.array([33, 255, 255])
+color_lower_yellow = np.array([23, 60, 100])
+color_upper_yellow = np.array([35, 255, 255])
 
-lower_neon_pink = np.array([140, 60, 60])
-upper_neon_pink = np.array([175, 255, 255])
+lower_neon_pink = np.array([150, 80, 90])
+upper_neon_pink = np.array([170, 255, 255])
 
 
-color_lower_green = np.array([50, 90, 100])
+color_lower_green = np.array([50, 80, 90])
 color_upper_green = np.array([75, 255, 255])
 
-color_lower_orange = np.array([10, 135, 100])
+color_lower_orange = np.array([10, 60, 90])
 color_upper_orange = np.array([22, 255, 255])
 
 # Start capturing video from the webcam
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(1400)
 
 # Centers for the points of interest
 centers1 = []  # top
 centers2 = []  # bottom
 
-# Keep track of distances for average
-#distance_sum = 0
-#distance_num = 0
+# Semaphore for sync
+sem = threading.Semaphore(1)
 
 #focal_length = (perceived_width * KNOWN_DISTANCE) / KNOWN_WIDTH
 
 def find_angles_and_display(frame, dist_yp, dist_go):
-    print("find_angles_and_display called")
-    top_angle = 0
-    bot_angle = 0
-
+    #print("find_angles_and_display called")
+    global top_angle, bot_angle
     # Angle calculation and display onto frame
 
     # Calculate the perceived static distances between the center of the stickers and the center of the object
@@ -107,29 +99,35 @@ def find_angles_and_display(frame, dist_yp, dist_go):
     
     if(perceived_top_dist1 and perceived_bot_dist1 and perceived_top_dist2 and perceived_bot_dist2 != 0):
         # Calculate the top angle using law of cosines
-        try:
-            top_angle = math.degrees(math.acos((dist_yp**2 - perceived_top_dist1**2 - perceived_top_dist2**2)/(-2*perceived_top_dist1*perceived_top_dist2)))
-            #top_angle = math.degrees(math.acos((50**2 - perceived_top_dist1**2 - perceived_top_dist2**2)/(-2*perceived_top_dist1*perceived_top_dist2)))
-            # Display top angle onto the frame
-            #cv2.putText(frame, f"Top Angle: {top_angle:.4f}°", (50, 150), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-            print("Top angle is ", top_angle)
-            # Reset top dists
-            perceived_top_dist1 = 0
-            perceived_top_dist2 = 0
-        except ValueError:
-            print("Error calculating top angle!\n")
+        if dist_yp != 0:
+            try:
+                top_angle = math.degrees(math.acos((dist_yp**2 - perceived_top_dist1**2 - perceived_top_dist2**2)/(-2*perceived_top_dist1*perceived_top_dist2)))
+                #top_angle = math.degrees(math.acos((50**2 - perceived_top_dist1**2 - perceived_top_dist2**2)/(-2*perceived_top_dist1*perceived_top_dist2)))
+                # Display top angle onto the frame
+                #cv2.putText(frame, f"Top Angle: {top_angle:.4f}°", (50, 150), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+                print("Top angle is ", top_angle)
+                # Reset top dists
+                perceived_top_dist1 = 0
+                perceived_top_dist2 = 0
+                dist_yp = 0
+            except ValueError:
+                print("Error calculating top angle!\n")
+                print(f"The yp distance is {dist_yp}")
         # Calculate the bottom angle using law of cosines
-        try:
-            bot_angle = math.degrees(math.acos((dist_go**2 - perceived_bot_dist1**2 - perceived_bot_dist2**2)/(-2*perceived_bot_dist1*perceived_bot_dist2)))
-            #bot_angle = math.degrees(math.acos((50**2 - perceived_bot_dist1**2 - perceived_bot_dist2**2)/(-2*perceived_bot_dist1*perceived_bot_dist2)))
-            # Display bottom angle onto the frame
-            #cv2.putText(frame, f"Bottom Angle: {bot_angle:.4f}°", (50,200), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)    
-            print("Bottom angle is ", bot_angle)
-            # Reset bottom dists
-            perceived_bot_dist1 = 0
-            perceived_bot_dist2 = 0
-        except ValueError:
-            print("Error calculating bottom angle!\n")
+        if dist_go != 0:
+            try:
+                bot_angle = math.degrees(math.acos((dist_go**2 - perceived_bot_dist1**2 - perceived_bot_dist2**2)/(-2*perceived_bot_dist1*perceived_bot_dist2)))
+                #bot_angle = math.degrees(math.acos((50**2 - perceived_bot_dist1**2 - perceived_bot_dist2**2)/(-2*perceived_bot_dist1*perceived_bot_dist2)))
+                # Display bottom angle onto the frame
+                #cv2.putText(frame, f"Bottom Angle: {bot_angle:.4f}°", (50,200), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)    
+                print("Bottom angle is ", bot_angle)
+                # Reset bottom dists
+                perceived_bot_dist1 = 0
+                perceived_bot_dist2 = 0
+                dist_go = 0
+            except ValueError:
+                print("Error calculating bottom angle!\n")
+                print(f"The go distance is {dist_go}")
     timestamp = time.time() - start_time
     # Add data to csv
     data[0].append(top_angle)
@@ -139,8 +137,8 @@ def find_angles_and_display(frame, dist_yp, dist_go):
 def find_object_centers(cx, cy, cw, ch, joint):
     global centers1
     global centers2
-    global prev_x1, prev_x2
-    global prev_y1, prev_y2
+    global prev_x1, prev_y1
+    global prev_x2, prev_y2
     global perceived_width
     # Calculate the center coordinates of the rectangle
     center_x = int(cx + cw / 2)
@@ -158,7 +156,7 @@ def find_object_centers(cx, cy, cw, ch, joint):
             # Sort so it doesn't sample the same rectangle twice
             if((not (prev_x1 - 8 <= center_x <= prev_x1 + 8) and not (prev_y1 - 8 <= center_y <= prev_y1 + 8))):
                 centers1.append((center_x, center_y))
-        elif len(centers1) == 2:
+        elif len(centers1) >= 2:
             centers1 = []
     # Find center for bottom objects
     else:
@@ -170,7 +168,7 @@ def find_object_centers(cx, cy, cw, ch, joint):
             # Sort so it doesn't sample the same rectangle twice
             if((not (prev_x2 - 8 <= center_x <= prev_x2 + 8) and not (prev_y2 - 8 <= center_y <= prev_y2 + 8))):
                 centers2.append((center_x, center_y))
-        elif len(centers2) == 2:
+        elif len(centers2) >= 2:
             centers2 = []
 
 def draw_centers_and_measure(centers, joint):
@@ -178,10 +176,6 @@ def draw_centers_and_measure(centers, joint):
     distance_pixels = 0
 
     if len(centers) >= 2:
-        # Draw the center of each object
-        #for center in centers:
-            #cv2.circle(frame, center, 2, (255, 0, 0), -1)
-        
         # Calculate and draw the line between centers
         #cv2.line(frame, centers[0], centers[1], (0, 255, 0), 2)
         
@@ -189,46 +183,54 @@ def draw_centers_and_measure(centers, joint):
         distance_pixels = np.linalg.norm(np.array(centers[0]) - np.array(centers[1]))
         distance_mm = distance_pixels * (KNOWN_WIDTH/perceived_width)
         if joint == 1:
+            print(f"Centers1 is {centers1}")
             centers1 = []
         else:
+            print(f"Centers2 is {centers2}")
             centers2 = []
     return distance_pixels
 def detect_color(combo, contours):
-    global flag
-    if flag == 0:
-        flag = -1 # Obtain flag
-        # Go through and find the distances between the centers of the objects (yellow and pink), (green and orange)
-        
-        for contour in contours:
-            area = cv2.contourArea(contour)
-            if ((area > min_area) and (area < max_area)):
-                # Object is larger than minimum area, so proceed with distance calculation
-                # Assuming you have the perceived width in pixels (e.g., from bounding box width)
-                x, y, w, h = cv2.boundingRect(contour)
-                perceived_width = w  # or use h for height, depending on known dimension
-                
-                # Draw the bounding box for visualization
-                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+    global sem
+    global dist_go
+    global dist_yp
+    sem.acquire()
+    # Go through and find the distances between the centers of the objects (yellow and pink), (green and orange)
+    
+    for contour in contours:
+        #print(contour)
+        area = cv2.contourArea(contour)
+        if ((area > min_area) and (area < max_area)):
+            # Object is larger than minimum area, so proceed with distance calculation
+            # Assuming you have the perceived width in pixels (e.g., from bounding box width)
+            x, y, w, h = cv2.boundingRect(contour)
+            perceived_width = w  # or use h for height, depending on known dimension
+            
+            # Draw the bounding box for visualization
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
-                if combo == 'yp':
-                    # Find the centers of objects
-                    find_object_centers(x, y, w, h, 1)
-                    # Measure the distance
-                    dist_yp = draw_centers_and_measure(centers1, 1)
-                    print(centers1)
-                    print(prev_x1)
-                    print(prev_y1)
-                    print(f"{combo} countour detected")
-                else:
-                    # Find the centers of objects
-                    find_object_centers(x, y, w, h, 2)
-                    # Measure the distance
-                    dist_go = draw_centers_and_measure(centers2, 2)
-                    print(centers2)
-                    print(prev_x2)
-                    print(prev_y2)
-                    print(f"{combo} countour detected")
-        flag = 0    # Release flag
+            if combo == 'go':
+                # Find the centers of objects
+                find_object_centers(x, y, w, h, 2)
+                # Measure the distance
+                dist_go = draw_centers_and_measure(centers2, 2)
+                #print(f"Centers2 is {centers2} and distance is {dist_go}")
+                #print(prev_x2)
+                #print(prev_y2)
+                #print(f"{combo} countour detected")
+                
+            else:
+                # Find the centers of objects
+                find_object_centers(x, y, w, h, 1)
+                # Measure the distance
+                dist_yp = draw_centers_and_measure(centers1, 1)
+                #print(f"Centers1 is {centers1} and distance are {dist_yp}")
+                #print(prev_x1)
+                #print(prev_y1)
+                #print(f"{combo} countour detected")
+            if dist_yp or dist_go != 0:
+                # Calculate and display the angles of each joint on the frame
+                find_angles_and_display(frame, dist_yp, dist_go)
+    sem.release()
 
 def process_frame(frame):
 
@@ -257,16 +259,14 @@ def process_frame(frame):
         thread = threading.Thread(target=detect_color, args=(combo, contour_value))
         threads.append(thread)
         thread.start()
-    
     for thread in threads:
         thread.join()
+
 
 
 if __name__ == "__main__":
     global start_time 
     start_time = time.time()
-    dist_yp = 0
-    dist_go = 0
     while True:
         # Capture a single frame
         ret, frame = cap.read()
@@ -275,9 +275,6 @@ if __name__ == "__main__":
         
         process_frame(frame)        # Process frame and create threads
         
-        if dist_yp <= 50 and dist_go <= 50:
-            # Calculate and display the angles of each joint on the frame
-            find_angles_and_display(frame, dist_yp, dist_go)
         # Display the resulting frame
         cv2.imshow('Frame', frame)
         
